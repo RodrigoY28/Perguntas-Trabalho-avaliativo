4fd9a50c45c63b26c2de828f601313771b708ca0
RodrigoY28 <161314706+RodrigoY28@users.noreply.github.com>
2024-11-11 07:48:11 -0300

    Add files via upload

---
+++ yShiro3Tri-main/yShiro3Tri/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é o C++?',
+    'O C++ é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Brasil?',
+    'A capital do Brasil é Brasília'
+)
+
+criaCartao(
+    'Html',
+    'Para que serve o Html?',
+    'O Html serve para definir o significado e a estrutura de um conteúdo da web'
+)
+
+criaCartao(
+    'Matemática',
+    'Quanto é 8 x 8?',
+    '8 x 8= 64'
+)
